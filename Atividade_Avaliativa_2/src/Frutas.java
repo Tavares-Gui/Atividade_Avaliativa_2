294f2ce64132b29f64d12a448d6fe45da420b451
Giovanna Rosa <102667311+rsgiovanna19@users.noreply.github.com>
2024-04-16 20:15:10 -0300

    Right files

---
+++ Atividade_Avaliativa_2/src/Frutas.java
@@ -0,0 +1,28 @@
+public class Frutas extends Produto {
+    private String tipoFruta;
+
+    
+    
+
+    public Frutas(String nome, float preco, int quantidadeEstoque, String dataValidade, int codigo) {
+        super(nome, preco, quantidadeEstoque, dataValidade, codigo);
+    }
+
+    public Frutas() {
+    }
+
+
+    //getters setters
+    public String getTipoFruta() {
+        return tipoFruta;
+    }
+
+    public void setTipoFruta(String tipoFruta) {
+        this.tipoFruta = tipoFruta;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
